0e51dfc6fbab59d00cfaab26c7e8b2795245eb80
vsh4535-cyber <vsh4535@gmail.com>
2026-02-23 09:56:45 +0000

    modified .js file

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
