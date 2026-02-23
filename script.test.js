00fdbb0d5cdc9cdb33feb7434405ab8495a34f80
vsh4535-cyber <vsh4535@gmail.com>
2026-02-23 10:37:26 +0000

    changes made in sonar

--- script.test.js
+++
@@ -1,8 +0,0 @@
-const { getRandomColor } = require('./script');
-
-describe('getRandomColor', () => {
-  test('returns a valid hex color', () => {
-    const color = getRandomColor();
-    expect(color).toMatch(/^#[0-9A-F]{6}$/);
-  });
-});
